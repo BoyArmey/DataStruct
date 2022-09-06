ab4cb9ac589169d8d1cd720e38d6f737019a9678
BoyArmey <1273971225@qq.com>
2022-09-06 20:14:55 +0800

    update

--- 1.线性表/2.链表/2.双向链表/DuLinkList.h
+++ 1.线性表/2.链表/2.双向链表/DuLinkList.h
@@ -1,5 +1,13 @@
 #ifndef _DULINKLIST_H_
 #ifdef _DULINKLIST_H_
 
+//定义链表中结点存储数据的类型
+typedef struct 
+{
+    char id[10];       //ID
+    char name[20];    //姓名
+    int age;          //年龄
+}ElemType;
+
 
 #endif
